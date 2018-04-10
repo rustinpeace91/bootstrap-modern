8b50a6fbc85cd982dae1e0cb7a21fbb22c154dc0
Isaac <isaaclubanko@gmail.com>
2018-04-10 10:07:47 -0400

    now responds to css. header.php reated

---
+++ footer.php
@@ -0,0 +1,20 @@
+    <!-- /.container -->
+    <?php
+/**
+ * The footer for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package Modern Business
+ */
+?>
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
